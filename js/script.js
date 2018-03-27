3ce7783283ee33335196fc0911cf2f7e41e6e5cf
parkersalterego <parkersalterego92@gmail.com>
2018-03-27 06:41:35 -0700

    menu animations polished and neon effect added to header logo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,45 @@
-const about = document.getElementById('about');
-const aboutBtn = document.getElementById('aboutBtn');
+(function(){
+    const menuButtons = [
+        document.getElementById('aboutBtn'), 
+        document.getElementById('workBtn'), 
+        document.getElementById('contactBtn')
+    ];
+    
+    const panelBackButtons = [
+        document.getElementById('aboutBackBtn'),
+        document.getElementById('workBackBtn'),
+        document.getElementById('contactBackBtn')
+    ];
 
-aboutBtn.addEventListener('click', function() {
-   if (about.classList.contains('panelOpen')) {
-        about.classList.remove('panelOpen');
-   } else {
-       about.classList.add('panelOpen');
-   }
-});
+    const panels = [
+        document.getElementById('about'), 
+        document.getElementById('work'), 
+        document.getElementById('contact')
+    ];
+
+    const headerLogo = document.getElementById('headerLogo');
+
+    for(let i = 0; i < menuButtons.length; i++) {
+
+        menuButtons[i].addEventListener('click', () => {
+            panels[i].classList.add('panel-open');
+            menuButtons.forEach((button) => {
+                button.style.opacity = 0;
+            });
+
+            headerLogo.style.opacity = 0;
+            headerLogo.firstChild.classList.remove('neon');
+        });
+
+        panelBackButtons[i].addEventListener('click', () => {
+
+            panels[i].classList.remove('panel-open');
+            menuButtons.forEach((button) => {
+                button.style.opacity = 1; 
+            });
+
+            headerLogo.style.opacity = 1;
+            headerLogo.firstChild.classList.add('neon');
+        });
+    }
+})();
